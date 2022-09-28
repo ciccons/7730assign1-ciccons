df16fe995fae3dd2d024e5f612ddb7302bad1de8
Stephanie Ciccone <ciccons1928@gmail.com>
2022-09-28 18:18:10 -0400

    revised forelse.py

--- codes/forelse.py
+++ codes/forelse.py
@@ -13,3 +13,19 @@ def look(target,names):
 names = ["Alice", "Bob", "Eve"]
 print(look("Eve", names))
 print(look("Jack", names))
+
+# Revised by Stephanie Ciccone
+# Numerical Methods in Physics with Python (CUP, 2020)
+
+def look(target,names):
+    for name in names:
+        if name==target:
+            val = name
+            break
+    else:
+        val = None
+    return val
+
+names = ["Stephanie", "Alex", "Kathleen"]
+print(look("Stephanie", names))
+print(look("Rachel", names))
